bdd3c40c161697f22bb2f4cedec4a539b08e16fb
划划水 <103646023+huahsui@users.noreply.github.com>
2023-04-14 17:10:26 +0800

    Update sni+xtls.sh

--- sni+xtls.sh
+++ sni+xtls.sh
@@ -226,7 +226,7 @@ sleep 2
 systemctl daemon-reload && systemctl restart xray && systemctl enable xray && systemctl restart nginx
 systemctl enable nginx && touch cronfile && echo '15 2 * */2 * root certbot renew --pre-hook "systemctl stop nginx" --post-hook "systemctl start nginx"' > ./cronfile && crontab -u root ./cronfile
 sleep 1
-wget -N --no-check-certificate -q -O /html/we.dog/$UUID.yaml "https://raw.githubusercontent.com/huahsui/tcp-xtls/gh-pages/clash.yaml" && sed -i '48 i\  - {name: tcp+xtls, server: '$DOMIN', port: 443, type: vless, uuid: '$UUID', flow: xtls-rprx-vision, skip-cert-verify: false, servername: '$DOMIN'}' /html/we.dog/$UUID.yaml
+wget -N --no-check-certificate -q -O /html/we.dog/$UUID.yaml "https://raw.githubusercontent.com/huahsui/tcp-xtls/gh-pages/clash.yaml" && sed -i '51 i\  - {name: tcp+xtls, server: '$DOMIN', port: 443, type: vless, uuid: '$UUID', flow: xtls-rprx-vision, skip-cert-verify: false, servername: '$DOMIN'}' /html/we.dog/$UUID.yaml
 sleep 1
 clear
 
